1e855a250cca94cf2c5b3085affe1b248cdc7c24
Franco Morales Castro <franco.morales@outlook.com>
2024-04-12 19:35:57 -0400

    feat: estrucutura

--- ejercicioCompleto/main.py
+++ ejercicioCompleto/main.py
@@ -7,3 +7,18 @@
 # Cada una de las opciones debe permitir al usuario realizar las operaciones
 # usando listas y diccionarios. Tambien debera repetir el programa hasta
 # que el usuario seleccione la opcion 5.
+
+def buscar_persona():
+    pass
+
+def agregar_persona():
+    pass
+
+def eliminar_persona():
+    pass
+
+def editar_persona():
+    pass
+
+def menu():
+    pass
