812a9b4e067f6dab8e98c15728cdfd6b061dc72b
Franco Morales Castro <franco.morales@outlook.com>
2024-04-12 20:15:42 -0400

    feat: funcionalidad del menu

--- ejercicioCompleto/main.py
+++ ejercicioCompleto/main.py
@@ -20,5 +20,36 @@ def eliminar_persona():
 def editar_persona():
     pass
 
+# Funcion que valida si una opcion es correcta
+# Si se encuentre etre 1 y 5
+def es_opcion_correcta(opcion: str) -> bool:
+    opciones = ("1", "2", "3", "4", "5")
+    if opcion in opciones:
+        return True
+    else:
+        return False
+
 def menu():
-    pass
+    # Mostrar el menu mientras el usuario lo requiera
+    while True:
+        # Se muestra el menu al usuario
+        print("MENU APLICACION")
+        print("1.- Buscar persona")
+        print("2.- Agregar persona")
+        print("3.- Eliminar persona")
+        print("4.- Editar persona")
+        print("5.- Salir")
+        # Se solicita al usuario que ingrese su opcion
+        opcion = input("Ingresa tu opcion: ")
+        # Validamos que la opcion sea correcta
+        while not es_opcion_correcta(opcion):
+            print("Error: Ud. ha ingresado un opcion incorrecta.")
+            opcion = input("Ingrese una opcion entre 1 y 5: ")
+
+        # Flujos segun la opcion
+        if opcion == "5":
+            break
+
+    print("Ud. ha terminado el programa.")
+
+menu()
